11e1cf36f536d8bff72804ff915e622cf2b9c6d2
Rustem Amirkhanuly <rekewka@Zholdassovas-MacBook-Air.local>
2025-03-21 19:03:21 +0500

    adding tasks

---
+++ project2/frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import { BrowserRouter as Router} from "react-router-dom"
+import RouterSet from "./RouterSet"
+
+function App() {
+
+  return (
+    <>
+      <Router>
+        <RouterSet />
+      </Router>
+    </>
+  )
+}
+
+export default App
---
+++ project2/frontend/src/pages/Products.tsx
@@ -0,0 +1,9 @@
+function Products(){
+    return(
+        <>
+            <h1>home works!</h1>
+        </>
+    )
+}
+
+export default Products
